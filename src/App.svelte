f2af4759f8535de0e6e7f164d2a82e0c535a1e4f
r3nanp <renanmol87@gmail.com>
2020-10-09 20:29:57 -0300

    feat(svelte script): add the styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
-<script>
+<script context="module">
+  import Form from './Main.svelte'
 </script>
 
 <style>
@@ -9,124 +10,9 @@
     align-items: center;
     justify-content: center;
   }
-
-  form {
-    --text-color: #afafaf;
-    max-width: 500px;
-  }
-
-  .field {
-    width: 100%;
-    position: relative;
-    border-bottom: 2px dashed var(--text-color);
-    margin: 4rem auto 1rem;
-  }
-
-  .label {
-    color: var(--text-color);
-    font-size: 1.2rem;
-  }
-
-  .input {
-    outline: none;
-    border: none;
-    overflow: hidden;
-    margin: 0;
-    width: 100%;
-    padding: 0.25rem 0;
-    background: none;
-    color: white;
-    font-size: 1.2rem;
-    font-weight: bold;
-  }
-
-  .input:valid {
-    color: yellowgreen;
-  }
-
-  .input:invalid {
-    color: orangered;
-  }
-
-  /* Animation */
-
-  .field::after {
-    content: '';
-    position: relative;
-    display: block;
-    height: 4px;
-    width: 100%;
-    background: #d16dff;
-    transform: scaleX(0);
-    transform-origin: 0%;
-    transition: transform 500ms ease;
-    top: 2px;
-  }
-
-  .field:focus-within {
-    border-color: transparent;
-  }
-
-  .field:focus-within::after {
-    transform: scaleX(1);
-  }
-
-  /* Label animation */
-
-  .label {
-    z-index: -1;
-    position: absolute;
-    transform: translateY(-2rem);
-    transform-origin: 0%;
-    transition: transform 400ms;
-  }
-
-  .field:focus-within .label,
-  .input:not(:placeholder-shown) + .label {
-    transform: scale(0.8) translateY(-5rem);
-  }
-
-  .strength {
-    display: flex;
-    height: 20px;
-    width: 100%;
-  }
-
-  .bar {
-    margin-right: 5px;
-    height: 100%;
-    width: 25%;
-    transition: box-shadow 500ms;
-    box-shadow: inset 0 20px #1f1f1f;
-  }
-
 </style>
 
+<!-- HTML-->
 <main>
-  <form>
-    <div class="field">
-      <input type="email" name="email" class="input" />
-      <label for="email" class="label">Email</label>
-    </div>
-
-    <div class="field">
-      <input type="password" class="input" placeholder="" />
-      <label for="password" class="label">Password</label>
-    </div>
-
-    <div class="strength">
-      <span class="bar bar-1" />
-      <span class="bar bar-2" />
-      <span class="bar bar-3" />
-      <span class="bar bar-4" />
-    </div>
-
-    <ul>
-      <li>must be at least 5 characters</li>
-      <li>must contain a capital letter</li>
-      <li>must contain a number</li>
-      <li>must contain one of special character</li>
-    </ul>
-
-  </form>
+  <Form />
 </main>
---
+++ src/Strength.svelte
@@ -0,0 +1,46 @@
+<script>
+  let strength = 0
+  let validations = []
+
+</script>
+
+<style>
+  .strength {
+    display: flex;
+    height: 20px;
+    width: 100%;
+  }
+
+  .bar {
+    margin-right: 5px;
+    height: 100%;
+    width: 25%;
+    transition: box-shadow 500ms;
+    box-shadow: inset 0 20px #1f1f1f;
+  }
+
+  .bar-show {
+    box-shadow: none;
+  }
+
+  .bar:nth-child(1) {
+    background: linear-gradient(to right, red, orangered);
+  }
+  .bar:nth-child(2) {
+    background: linear-gradient(to right, orangered, yellow);
+  }
+  .bar:nth-child(3) {
+    background: linear-gradient(to right, yellow, yellowgreen)
+  }
+  .bar:nth-child(4) {
+    background: linear-gradient(to right, yellowgreen, green)
+  }
+
+</style>
+
+<div class="strength">
+  <span class="bar" />
+  <span class="bar" />
+  <span class="bar" />
+  <span class="bar" />
+</div>
